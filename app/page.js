e0903439723cff306ff7b4e9db3c9c27c8c3da57
yamineHjr <yasmine.alnajar@gmail.com>
2024-11-14 02:17:03 +0200

    home page and  initial routes

--- app/page.js
+++ app/page.js
@@ -1,9 +1,28 @@
-import Image from "next/image";
+import styles from "./style";
+import { NavBar, Hero, Stats, PopularCars, PopularHotels, Testimonials, Footer, CTA, HaveToChoose } from "@/components";
 
 export default function Home() {
   return (
-    <div className="bg-dark-700">
-      <h1 className="text-white">الحمد لله</h1>
-    </div>
-      );
+    <div className="bg-primary w-full overflow-hidden">
+  
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero/>
+        </div>
+      </div>
+  
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats />
+          <PopularCars/>
+          <PopularHotels/>
+          <HaveToChoose/>
+          <Testimonials/>
+          <CTA/>
+          {/* <Footer /> */}
+        </div>
+      </div>
+
+  </div>
+  );
 }
