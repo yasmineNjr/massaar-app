fbbe0537088d024145dd19b8605f53fb18eaed82
yamineHjr <yasmine.alnajar@gmail.com>
2024-11-16 13:41:06 +0200

    dropdown navbar menu

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -4,6 +4,14 @@ import { useState } from 'react';
 import { close, logo, menu } from '../public/assets'
 import { navLinks } from '../constants';
 import Image from 'next/image';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 function NavBar() {
 
@@ -18,17 +26,30 @@ function NavBar() {
       <Image src={logo} alt='algharbia' className='w-[80px] h=[32px]'/>
       <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
         {navLinks.map((nav, index) => (
-          <li key={nav.id} 
-          className={`font-normal cursor-pointer text-[16px] text-dimWhite ${index === navLinks.length-1 ?' ml-0' : 'ml-10'}`}>
-            <a href={`${nav.link}`}>{nav.title}</a>
-          </li>
+          nav.id !== 'reservations' ?
+            <li key={nav.id} 
+            className={`font-normal cursor-pointer text-[16px] text-dimWhite ${index === navLinks.length-1 ?' ml-0' : 'ml-10'}`}>
+              <a href={`${nav.link}`}>{nav.title}</a>
+            </li> :
+             <li
+              key={nav.id} 
+              className={`font-normal cursor-pointer text-[16px] text-dimWhite ${index === navLinks.length-1 ?' ml-0' : 'ml-10'}`}>
+              {/* <a href={`${nav.link}`}>{nav.title}</a> */}
+              <DropdownMenu>
+                <DropdownMenuTrigger  className='text-dimWhite'>{nav.title}</DropdownMenuTrigger>
+                  <DropdownMenuContent className='bg-black-gradient text-dimWhite'>
+                    <DropdownMenuItem><div className='w-full text-center'><a href='/hotels'>حجز فندقي</a> </div></DropdownMenuItem>
+                    <DropdownMenuItem><div className='w-full text-center'><a href='/cars'>حجز سيارة</a> </div></DropdownMenuItem>
+                  </DropdownMenuContent>
+              </DropdownMenu>
+              </li>
         ))}
+       
+        
       </ul>
+     
       <div className='sm:hidden flex flex-1 justify-end items-center'>
-          {/* for gradient background  */}
-          <div className='absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient'/>
-          <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
-          <div className="absolute z-[0] w-[50%] h-[50%] left-20 bottom-20 white__gradient" />
+          
         <Image 
           src={toggle ? close : menu} 
           alt='menu' 
@@ -47,6 +68,8 @@ function NavBar() {
         </div>
       </div>
     </nav>
+
+     
   )
 }
 
