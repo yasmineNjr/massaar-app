9e2c2ca2a939bcaa58b3221363bdc4833c536e49
yamineHjr <yasmine.alnajar@gmail.com>
2024-11-17 18:31:01 +0200

    initial reservations booking page

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,8 +1,47 @@
 import React from 'react'
+import styles from '../style'
+import StatCard from '@/components/StatCard'
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div className="w-full overflow-hidden">
+  
+        <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+            <div className={`${styles.boxWidth}`}>
+                <div className="text-center">
+                    <h2 className={`${styles.heading2} text-right`}>
+                    إدارة الطلبات    
+                    </h2>
+                </div>
+                <p>ابدأ يومك بمعالجة طلبات الحجز</p>
+            
+            <section className='mx-auto flex flex-col space-y-14'> 
+                <main className='admin-main'>
+                  <section className='admin-stat'>
+                  <StatCard
+                      type="appointments"
+                      count={5}//{appointments.scheduledCount}
+                      label="Scheduled appointments"
+                      icon={"/assets/icons/appointments.svg"}
+                  />
+                   <StatCard
+                      type="pending"
+                      count={2}//{appointments.pendingCount}
+                      label="Pending appointments"
+                      icon={"/assets/icons/pending.svg"}
+                  />
+                  <StatCard
+                      type="cancelled"
+                      count={3}//{appointments.cancelledCount}
+                      label="Cancelled appointments"
+                      icon={"/assets/icons/cancelled.svg"}
+                  />
+                  </section>
+                </main>
+            </section> 
+      </div>
+    </div>
+  </div>
   )
 }
 
---
+++ components/StatCard.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const StatCard = ({ count = 0, label, icon, type }) => {
+  return (
+    <div className=''
+    // {clsx('stat-card', {
+    //     "bg-appointments": type === "appointments",
+    //     "bg-pending": type === "pending",
+    //     "bg-cancelled": type === "cancelled",
+    //   })}
+      >
+        {/* <div className='flex items-center gap-4'>
+          <Image
+            src={icon}
+            width={32}
+            height={32}
+            className='size-8 w-fit'
+            alt={label}
+          />
+          <h2 className='text-32-bold text-white'>{count}</h2>
+        </div>
+  
+        <p className='text-14-regular'>{label}</p> */}
+        xxx
+      </div>
+  )
+}
+
+export default StatCard
