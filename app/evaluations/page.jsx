91bff91422f919ecd98a034e8a267c793e31d2ff
yamineHjr <yasmine.alnajar@gmail.com>
2024-11-18 06:35:19 +0200

    initial client review page

--- app/evaluations/page.jsx
+++ app/evaluations/page.jsx
@@ -1,10 +1,51 @@
+import { hero1 } from '@/public/assets'
 import React from 'react'
+import styles, { layout } from '../style'
+import Image from 'next/image'
+import ClientEvaluationForm from '@/components/forms/ClientEvaluationForm'
 
 const CustomerEvaluations = () => {
   return (
-    <div className='text-white'>
-      CustomerEvaluations
-    </div>
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <div className="text-center my-10">
+            <h2 className={`${styles.heading2} text-right`}>
+                تقييم العملاء
+            </h2>
+          </div>
+          
+          <section className={`${styles.paddingY} flex flex-col`}>
+
+            <div className='flex flex-row w-full'>
+              {/* <div className={`hidden md:block flex-1 flex-col ${styles.flexCenter} xl:px-0 sm:px-16 px-6 md:my-0 my-5 relative h-[525px] lg:w-[50%] w-[100%] `}> */}
+                {/* <div className="w-full max-w-4xl mx-auto"> */}
+                  
+                  <div className="relative h-64 md:h-96 w-[50%]">
+                    <Image
+                      src={hero1}
+                      alt="Evaluation Image"
+                      layout="fill"
+                      objectFit="cover"
+                      // className="rounded-lg"
+                    />
+                  </div>
+                {/* </div> */}
+              {/* </div> */}
+              <div className={`${styles.flexCenter} xl:px-0 sm:px-16 px-6 md:my-0 my-10 lg:w-[50%] w-[100%]`}>
+                <ClientEvaluationForm/>             
+              </div>
+            </div>
+
+            <div className={`${styles.flexCenter}   bg-white`}>
+              evaluation list
+            </div>
+
+          </section>
+        </div>
+      </div>
+  </div>
+
   )
 }
 
---
+++ components/forms/ClientEvaluationForm.jsx
@@ -0,0 +1,85 @@
+'use client'
+
+import { useForm } from "react-hook-form";
+import { Form, FormControl } from "../ui/form"
+import CustomFormField from "../CustomFormFeild";
+import Button from "../Button";
+import { useState } from "react";
+
+export const FormFieldType = {
+    INPUT: 'input',
+    TEXTAREA: 'textarea',
+    PHONE_INPUT: 'phoneInput',
+    CHECKBOX: 'checkbox',
+    DATE_PICKER: 'datePicker',
+    SELECT: 'select',
+    SKELETON: 'skeleton',
+  };
+
+const ClientEvaluationForm = () => {
+
+    const form = useForm();
+
+    const onSubmit = (data) => {
+        console.log(data);
+      };
+
+    const [rating, setRating] = useState(0);
+
+    const handleRating = (value) => {
+    setRating(value);
+    };
+
+  return (
+    <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="font-bold space-y-6 flex flex-1 flex-col w-full text-dimWhite ">
+            <div className="flex lg:flex-row flex-col">
+                <div className="w-[100%] px-3 space-y-6">
+                   
+                    <CustomFormField
+                        fieldType={FormFieldType.INPUT}
+                        control={form.control}
+                        name="name"
+                        label="الاسم الثلاثي"
+                        placeholder="أكتب اسمك الثلاثي هنا"
+                        iconSrc="/assets/user.svg"
+                        iconAlt="user"
+                    />
+                     <CustomFormField
+                        fieldType={FormFieldType.PHONE_INPUT}
+                        control={form.control}
+                        name="phone"
+                        label="رقم الجوال"
+                        placeholder="(555) 123-4567"
+                    /> 
+                     <CustomFormField
+                        fieldType={FormFieldType.INPUT}
+                        control={form.control}
+                        name="email"
+                        label="البريد الالكترني(اختياري)"
+                        placeholder="johndoe@gmail.com"
+                        iconSrc="/assets/email.svg"
+                        iconAlt="email"
+                    />
+                    <CustomFormField
+                        fieldType={FormFieldType.TEXTAREA}
+                        control={form.control}
+                        name="description"
+                        label="الوصف(إختياري)"
+                        placeholder="أكتب ملاحظاتك عن خدماتنا"
+                    />
+                    
+                  
+                </div>
+
+            </div>
+            
+            <div className="flex flex-1 justify-center items-center w-fill mt-6">
+                <Button styles='lg:w-[50%] w-[100%]' title='تقييم'/>
+            </div>
+        </form>
+    </Form>
+  )
+}
+
+export default ClientEvaluationForm
