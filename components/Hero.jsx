32fccbcd3c6d13034b8acfdf454f1a73b3ace4dd
yamineHjr <yasmine.alnajar@gmail.com>
2024-11-28 15:22:28 +0200

    update the logo in hero section

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,6 +2,7 @@ import styles from '@/app/style';
 import Image from "next/image";
 import QuickBook from './QuickBook';
 import { heroImages } from '@/constants';
+import { logo } from '@/public/assets';
 
 const Hero = () => (
   <>
@@ -9,7 +10,7 @@ const Hero = () => (
     className={`flex flex-1 flex-col h-[700px] ${styles.paddingY} bg-transparent  justify-center items-center flex-col xl:px-0 sm:px-16 px-6`}
     style={{ backgroundImage: "url('/assets/home.jpg')", backgroundRepeat: "no-repeat", backgroundSize: "cover", backgroundPosition: "center",}}>
 
-        <div className="flex flex-row justify-between items-center w-full text-center">
+        {/* <div className="flex flex-row justify-between items-center w-full text-center">
           <h1 className="flex-1 font-poppins font-semibold ss:text-[42px] text-[22px] text-primary ss:leading-[100.8px] leading-[75px]">
             <span className="text-gradient  animate-fadeIn">مسار الغربية</span>{" "}
             <br className="sm:block hidden" />
@@ -18,7 +19,10 @@ const Hero = () => (
         </div>
         <h1 className="font-poppins font-semibold ss:text-[38px] text-[22px] text-primary ss:leading-[100.8px] leading-[75px] w-full text-center">
           خدمات رحلات الحج و العمرة
-        </h1>
+        </h1> */}
+        <div className='mb-10'>
+          <Image src={logo} alt='algharbia' className='w-full max-w-[370px] h-auto'/>
+        </div>
             
         <div className="ss:flex md:mr-4 mr-0 bg-transparent">
           <QuickBook/>
