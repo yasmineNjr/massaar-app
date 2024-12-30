e22bed31a0ff670d4a681c8400ce0719ae41073a
yamineHjr <yasmine.alnajar@gmail.com>
2024-12-30 19:48:48 +0200

    update testimonials

--- components/FeedbackCard.jsx
+++ components/FeedbackCard.jsx
@@ -3,9 +3,9 @@ import { quotes , star} from '../public/assets'
 
 const FeedbackCard = ({ content, name, title, img, date }) =>   (
   ////feedback-card
-    <div className='flex justify-between flex-col px-10 py-12 rounded-[20px] max-w-[250px] md:mr-10 sm:mr-5 mr-0 my-5 hover:bg-gradient-to-r from-gray-300 to-white hover:shadow-customGray'>
+    <div className='flex justify-between flex-col px-10 py-12 rounded-[20px] max-w-[250px] md:mr-10 sm:mr-5 mr-0 my-5 hover:bg-gradient-to-r from-gray-300 to-white hover:shadow-customGray text-dimWhite hover:text-primary'>
       <Image src={quotes} alt='double-quotes' className='w-[42px] h-[27px] object-contain '/>
-      <p className='font-poppins font-normal text-[18px] leading-[32px] text-customSecondary my-10'>
+      <p className='font-poppins font-normal text-[18px] leading-[32px] my-10'>
         {content}
       </p>
       <div className='flex flex-row'>
@@ -17,8 +17,8 @@ const FeedbackCard = ({ content, name, title, img, date }) =>   (
       </div>
       <div className='flex flex-row'>
         <div className='flex flex-col'>
-          <h4 className='font-poppins font-semibold text-[20px] leading-[32px] text-customSecondary'>{name}</h4>
-          <p className='font-poppins font-normal text-[16px] leading-[24px] text-customSecondary'>{date}</p>
+          <h4 className='font-poppins font-semibold text-[20px] leading-[32px] '>{name}</h4>
+          <p className='font-poppins font-normal text-[16px] leading-[24px] '>{date}</p>
         </div>
       </div>
     </div>
